219c7038e3be3cc97516578ddd30bd4813822547
Yifan J <yjin392@gatech.edu>
2022-03-26 16:12:24 -0700

    update the add popup window
    Fix the bug

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -14,8 +14,14 @@ const AddPost = ({onAdd, onSave}) => {
         e.preventDefault()
         if (!title) {
             alert('please add a task')
-        }
-        onAdd({title, Price, Description, Contact, Location, image})
+        } else if (!Price) {
+            alert('please enter the price')
+        } else if (!Location) {
+            alert('Please enter the location')
+        } else if (!Contact) {
+            alert('Please enter the contact')
+        } else {
+            onAdd({title, Price, Description, Contact, Location, image})
 
         setTitle('')
         setPrice('')
@@ -25,7 +31,7 @@ const AddPost = ({onAdd, onSave}) => {
         setContact('')
 
         onSave()
-
+        }
     }
 
 
@@ -59,7 +65,7 @@ const AddPost = ({onAdd, onSave}) => {
 
                 <Form.Group className="mb-3" >
                     <Form.Label>Image: </Form.Label>
-                    <Form.Control type='image' placeholder='Add Image' alt="image" value = {image} onChange={(e) => setImage(e.target.value)}/>
+                    <Form.Control type='file' placeholder='Add Image' alt="image" value = {image} onChange={(e) => setImage(e.target.value)}/>
                 </Form.Group>
 
                 <Button variant="primary" type='submit' value='Save post' className='btn btn-block'>
